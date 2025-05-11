071a4bcebaec7b9e9f08b8d7ed0fbda9ee17baaa
Hp372000 <harshilhemalpatel@gmail.com>
2025-05-10 21:47:08 -0400

    feat: add /api/health endpoint

--- backend/main.go
+++ backend/main.go
@@ -7,19 +7,19 @@ import (
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Backend Service is healthy! (Environment: %s)", os.Getenv("ENV"))
+	fmt.Fprintln(w, "Backend API Gateway is healthy!")
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" // default port
+		port = "8080"
 	}
 
 	http.HandleFunc("/api/health", healthHandler)
 
-	fmt.Println("Backend server starting on port", port)
-	err := http.ListenAndServe(":" + port, nil)
+	fmt.Println("Backend API Gateway starting on port", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
