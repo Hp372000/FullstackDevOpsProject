c74e648a865ac9ba24029de51704e548b81d5dc1
Hp372000 <harshilhemalpatel@gmail.com>
2025-07-27 22:46:07 -0400

    feat: add ECS Cluster module with execution role

--- infra/ecs/outputs.tf
+++ infra/ecs/outputs.tf
@@ -1,3 +1,8 @@
 output "cluster_arn" {
   value = module.ecs_cluster.cluster_arn
 }
+
+output "execution_role_arn" {
+  description = "ECS task execution role ARN"
+  value       = module.ecs_execution_role.role_arn
+}
--- tf_modules/ecs-cluster/main.tf
+++ tf_modules/ecs-cluster/main.tf
@@ -1,3 +1,10 @@
 resource "aws_ecs_cluster" "this" {
   name = var.cluster_name
 }
+
+module "network" {
+  source              = "../../tf_modules/vpc-basic"
+  name                = "fullstack"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+}
